377dccdb537a4800ba6daa7a83e28fff7a0d17b0
Hamza <shaikhhamzasaleem960@gmail.com>
2021-12-22 18:28:13 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+),
   <React.StrictMode>
     <App />
   </React.StrictMode>,
